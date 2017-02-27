d4f50136967dd8a8acb8ed59fd07c1775d0142d8
Mihai Cristescu <mihai.cristescu@gmail.com>
2017-02-27 16:46:58 +0200

    Add small changes.

--- RxSwift.playground/Contents.swift
+++ RxSwift.playground/Contents.swift
@@ -4,45 +4,48 @@ import RxSwift
 
 PlaygroundPage.current.needsIndefiniteExecution = true
 
+func delay(_ seconds: TimeInterval, task: @escaping ()->() ) {
+    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds, execute: task)
+}
 
 func printPrettyBox(message: String) {
     let length = message.characters.count
+    
     print("\n┌", terminator: "")
-    (1...length+2).forEach { _ in print("─", terminator: "") }
-    print("┐", terminator: "")
-    print("\n│ \(message) │", terminator: "")
-    print("\n└", terminator: "")
-    (1...length+2).forEach { _ in print("─", terminator: "") }
+    
+    for _ in 1...length + 2 {
+        print("─", terminator: "")
+    }
+    
+    print("┐\n│ \(message) │\n└", terminator: "")
+    
+    for _ in 1...length + 2 {
+        print("─", terminator: "")
+    }
+    
     print("┘\n")
 }
 
-delay(0) {
+/* Stop playground execution after delay [seconds] */
+
+delay(2) {
     printPrettyBox(message: "Playground has finished the execution.")
     PlaygroundPage.current.finishExecution()
-}
-
-func delay(_ seconds: TimeInterval, task: @escaping ()->() ) {
-    DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + seconds, execute: task)
-    
 }
 
 let a = Variable(1)
 let b = Variable(2)
 
-
 let c = Observable.combineLatest(a.asObservable(), b.asObservable()) { $0 + $1 }
     .filter { $0 > 0 }
-    .map { "\($0) is positive." }
+    .map { "Generated a positive sum (a+b): \($0)" }
 
 c.subscribe(onNext: { (result) in
     print(result)
-}, onError: { (error) in
-    print(error)
-}, onCompleted: { 
-    print("Gata")
-}, onDisposed: {
-    print("On dispose")
-})
+}, onError: nil, onCompleted: nil, onDisposed: nil)
 
-a.value = 2
+a.value = 2         // It triggers an event
 
+b.value = -5        // It doesn't trigger an event
+
+b.value = 0         // It triggers an event
